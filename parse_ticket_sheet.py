a79248e335edb5163fb3dc5cf7d6502af6b1f429
WillB97 <will@rudi.org.uk>
2021-09-01 23:24:02 +0100

    v0.9

---
+++ parse_ticket_sheet.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+import re
+import csv
+import sys
+from typing import Dict, List
+from pathlib import Path
+from datetime import datetime
+
+
+## Field conversion functions ##
+def simplify_product(value: str) -> str:
+    value = value.replace('Weekend', 'w/e')
+    value = value.replace('- Day Ticket', '')
+    value = value.replace('Ticket', '')
+    return value.strip()
+
+
+def simplify_date(value: str) -> str:
+    value_clean = re.sub(r'([0-9]+)(st|nd|rd|th)', r'\1', value)
+    date_value = datetime.strptime(value_clean, '%A %B %d %Y %H:%M %p')
+    return date_value.strftime('%a %d/%m')
+
+
+def tidy_price(value: str) -> str:
+    return value.replace('&pound;', '')
+
+
+# TODO different format w/ prices
+def parse_ticket_types(value: str) -> str:
+    ticket_strings = []
+    tickets = value.splitlines()  # convert "Price categories" field to a list of tickets
+
+    for ticket in tickets:
+        # each ticket line starts in the format: <ticket name>: <quantity> (£<price>)
+        ticket_fields = ticket.split()
+        ticket_name = ticket_fields[0][:-1]
+        ticket_qty = int(ticket_fields[1])
+        # ticket_price = float(ticket_fields[2][2:-1])
+
+        ticket_strings.append(f"{ticket_name[0]}:{ticket_qty}")
+
+    return ', '.join(ticket_strings)
+
+
+## Output configuration ##
+table_configuration = [
+    # (<input column heading>, <output column label>, <optional conversion function>),
+    ('Order ID', 'Order', None),
+    ('Booking ID', 'Booking', None),
+    ('Customer first name', 'First name', None),
+    ('Customer last name', 'Last name', None),
+    ('Quantity', 'Qty.', None),
+    (None, 'Issued', None),
+    (None, 'Infants', None),
+    (None, 'QR?', None),
+    ('Product price', 'Paid', tidy_price),
+    ('Price categories', 'Price categories', None),
+]
+
+column_sorts = {  # Use input column labels
+    'Booking ID': 'ASC',
+    'Start date': 'DATE',
+}
+
+GROUP_BOOKINGS_BY_DATE = True
+BOOKING_FILTER_STRING = 'Day Rover'  # Only products containing this substring will be included in the output
+
+# ===========================
+## Internal logic ##
+
+
+def parse_args():
+    error_string = f'Usage: {sys.argv[0]} <input-csv-file> <output-csv-file>'
+
+    if len(sys.argv) != 3:
+        print(error_string)
+        exit(1)
+
+    if not (Path(sys.argv[1]).is_file()):
+        print(error_string)
+        exit(1)
+
+
+def date_sort_item(date_str: str) -> datetime:
+    value_clean = re.sub(r'([0-9]+)(st|nd|rd|th)', r'\1', date_str)
+    return datetime.strptime(value_clean, '%A %B %d %Y %H:%M %p')
+
+
+def sort_bookings(bookings: List[List[str]], input_columns: List[str]) -> List[List[str]]:
+    for sort_column, direction in column_sorts.items():
+        sort_index = input_columns.index(sort_column)
+        if direction == 'DATE':
+            bookings.sort(key=lambda x: date_sort_item(x[sort_index]))
+        else:
+            bookings.sort(reverse=(direction == 'DESC'), key=lambda x: x[sort_index])
+    return bookings
+
+
+def filter_booking(booking: Dict[str, str]) -> bool:
+    return (booking['Product title'].find(BOOKING_FILTER_STRING) != -1)
+
+
+def format_booking_row(booking: Dict[str, str]) -> List[str]:
+    booking_output = []
+
+    for input_column, label, conversion in table_configuration:
+        if input_column is None:
+            booking_output.append('')
+            continue
+
+        field_value = booking[input_column]
+
+        if conversion is not None:
+            field_value = conversion(field_value)
+
+        booking_output.append(field_value)
+
+    return booking_output
+
+
+def date_suffix(day: int) -> str:
+    if 10 <= day <= 13:
+        return 'th'
+    else:
+        return {1: 'st', 2: 'nd', 3: 'rd'}.get(day % 10, 'th')
+
+
+def format_group_date(date: datetime) -> str:
+    day = date.day
+    date_partial = date.strftime('%A %B')  # day of week & month
+
+    return f"{date_partial} {day}{date_suffix(day)}".upper()
+
+
+def main():
+    output_bookings = []
+    last_seen_date = datetime(1970, 1, 1, 0, 0)  # use minimum date so the first date in printed
+
+    with open(sys.argv[1], 'r', errors='ignore') as f:
+        data_list = list(csv.reader(f, delimiter=','))
+
+        if not data_list:
+            print("No CSV rows found")
+            exit(1)
+
+    labels = data_list[0]  # top row is labels
+
+    bookings = sort_bookings(data_list[1:], labels)
+
+    for row in bookings:
+        booking = dict(zip(labels, row))  # map columns to label names
+        if filter_booking(booking):
+            output_bookings.append([format_booking_row(booking), booking])
+
+    with open(sys.argv[2], 'w', newline='') as f:  # output data into a new csv
+        output = csv.writer(f, quoting=csv.QUOTE_ALL)
+
+        output.writerow([column[1] for column in table_configuration])  # write header row
+
+        for booking, original_booking in output_bookings:
+            if GROUP_BOOKINGS_BY_DATE:
+                booking_date = date_sort_item(original_booking['Start date'])
+                if booking_date != last_seen_date:
+                    output.writerow(['', format_group_date(booking_date)])
+                    last_seen_date = booking_date
+            output.writerow(booking)
+
+
+if __name__ == '__main__':
+    parse_args()
+    main()
---
+++ server.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+import csv
+import json
+import requests
+from io import TextIOWrapper
+from flask import Flask, Markup, url_for, request, redirect, render_template
+from datetime import datetime
+
+import parse_ticket_sheet
+
+app = Flask(__name__)
+
+CONFIG_FILE = 'config.json'
+
+FILTER_STRING = ''
+CSV_URL = ''
+HIDE_OLD_ORDERS = False
+OLD_ORDER_DATE = ''
+
+
+def insert_html_newlines(value: str) -> str:
+    return Markup(value.replace('\n', '<br>'))
+
+
+table_configuration = [
+    # (<input column heading>, <output column label>, <optional conversion function>),
+    ('Order ID', 'Order', None),
+    ('Booking ID', 'Booking', None),
+    ('Customer first name', 'First name', None),
+    ('Customer last name', 'Last name', None),
+    ('Quantity', 'Qty.', None),
+    (None, 'Issued', None),
+    (None, 'Infants', None),
+    (None, 'QR?', None),
+    ('Product price', 'Paid', parse_ticket_sheet.tidy_price),
+    ('Price categories', 'Price categories', insert_html_newlines),
+]
+
+column_align = {
+    'Order': 'center',
+    'Booking': 'center',
+    'First name': 'right',
+    'Last name': 'left',
+    'Qty.': 'center',
+    'Issued': 'center',
+    'Infants': 'center',
+    'QR?': 'center',
+    'Paid': 'center',
+    'Price categories': 'left',
+}
+
+
+def parse_bookings(raw_data):
+    parsed_bookings = []
+    earliest_order_date = datetime.strptime(OLD_ORDER_DATE, '%Y-%m-%d')
+    labels = raw_data[0]  # top row is labels
+
+    bookings = parse_ticket_sheet.sort_bookings(raw_data[1:], labels)
+
+    for row in bookings:
+        booking = dict(zip(labels, row))  # map columns to label names
+
+        if HIDE_OLD_ORDERS:  # filter bookings by date
+            if parse_ticket_sheet.date_sort_item(booking['Start date']) < earliest_order_date:
+                continue
+
+        if parse_ticket_sheet.filter_booking(booking):
+            parsed_bookings.append([parse_ticket_sheet.format_booking_row(booking), booking])
+
+    return parsed_bookings
+
+
+def prepare_booking_table_values(processed_bookings, header):
+    rendered_bookings = []
+    last_seen_date = datetime(1970, 1, 1, 0, 0)  # use minimum date so the first date in printed
+
+    for booking, original_booking in processed_bookings:
+        if parse_ticket_sheet.GROUP_BOOKINGS_BY_DATE:
+            booking_date = parse_ticket_sheet.date_sort_item(original_booking['Start date'])
+            if booking_date != last_seen_date:
+                rendered_bookings.append({
+                    'booking_type': 'date',
+                    'date': parse_ticket_sheet.format_group_date(booking_date),
+                })
+                last_seen_date = booking_date
+
+        rendered_bookings.append({
+            'booking_type': 'order',
+            'booking': dict(zip(header, booking)),
+        })
+
+    return rendered_bookings
+
+
+def render_order_table(orders):
+    if not orders:
+        return "<p>No Ticket Data Found!</p>"
+
+    header = [column[1] for column in parse_ticket_sheet.table_configuration]
+
+    parsed_bookings = parse_bookings(orders)
+    rendered_bookings = prepare_booking_table_values(parsed_bookings, header)
+
+    return render_template(
+        'index.html',
+        header=header,
+        bookings=rendered_bookings,
+        align=column_align,
+        columns=len(header),
+        config={
+            'csv_url': CSV_URL,
+            'filter': FILTER_STRING,
+            'hideOld': HIDE_OLD_ORDERS,
+            'old_date': OLD_ORDER_DATE,
+        }
+    )
+
+
+@app.route('/')
+def ticket_sheet():
+    # Setup column layout & filter
+    parse_ticket_sheet.table_configuration = table_configuration
+    parse_ticket_sheet.BOOKING_FILTER_STRING = FILTER_STRING
+
+    # with open(TICKET_DUMP_FILE, 'r', errors='ignore') as f:
+    #     data_list = list(csv.reader(f, delimiter=','))
+
+    r = requests.get(CSV_URL)
+
+    if r.status_code != 200:
+        return "<p>Failed to fetch CSV data</p>"
+
+    data_list = list(csv.reader(r.text.splitlines(keepends=True), delimiter=','))
+
+    return render_order_table(data_list)
+
+
+@app.route('/manual', methods=['POST'])
+def uploaded_tickets():
+    # Setup column layout & filter
+    parse_ticket_sheet.table_configuration = table_configuration
+    parse_ticket_sheet.BOOKING_FILTER_STRING = FILTER_STRING
+
+    f = request.files['fileupload']
+
+    csv_str = TextIOWrapper(f).read()
+
+    data_list = list(csv.reader(csv_str.splitlines(keepends=True), delimiter=','))
+
+    return render_order_table(data_list)
+
+
+@app.route('/config', methods=['POST'])
+def update_config():
+    global FILTER_STRING, CSV_URL, HIDE_OLD_ORDERS, OLD_ORDER_DATE
+
+    # store request data
+    CSV_URL = request.form.get('csvUrl', '')
+    FILTER_STRING = request.form.get('filter', '')
+    HIDE_OLD_ORDERS = (request.form.get('hideOld', '') == 'hide')
+    OLD_ORDER_DATE = request.form.get('filterDate', '')
+
+    save_config()
+    return redirect(url_for('ticket_sheet'))
+
+
+def save_config():
+    config = {
+        "product filter": FILTER_STRING,
+        "CSV URL": CSV_URL,
+        "hide old orders": HIDE_OLD_ORDERS,
+        "old order date": OLD_ORDER_DATE,
+    }
+
+    print(f"New config: {config}")
+
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump(config, f, indent=4)
+
+
+def load_config():
+    global FILTER_STRING, CSV_URL, HIDE_OLD_ORDERS, OLD_ORDER_DATE
+
+    with open(CONFIG_FILE, 'r') as f:
+        config_data = json.load(f)
+
+    FILTER_STRING = config_data['product filter']
+    CSV_URL = config_data['CSV URL']
+    HIDE_OLD_ORDERS = config_data['hide old orders']
+    OLD_ORDER_DATE = config_data['old order date']
+
+
+load_config()
+
+if __name__ == "__main__":
+    app.run(debug=True)
